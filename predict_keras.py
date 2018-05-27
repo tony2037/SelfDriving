e6dec27be7d99b020eaab20780e4d9df4a5c3b75
tony2037 <ztex030640417@gmail.com>
2018-05-27 12:29:27 +0800

    add predict_keras

--- predict_keras.py
+++ predict_keras.py
@@ -1,1 +1,64 @@
-from keras.models import Model
+from keras.models import Model, Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, UpSampling2D, Conv2DTranspose
+import cv2
+
+def create_model():
+    image_size = (224,224,3)
+    model = Sequential()
+    model.add(Conv2D(64, (3, 3),strides=(1, 1), padding='same', input_shape=image_size))
+    model.add(Conv2D(64, (3, 3),strides=(1, 1), padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
+    model.add(Conv2D(128, (3, 3),strides=(1, 1), padding='same'))
+    model.add(Conv2D(128, (3, 3),strides=(1, 1), padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
+    model.add(Conv2D(256, (3, 3),strides=(1, 1), padding='same'))
+    model.add(Conv2D(256, (3, 3),strides=(1, 1), padding='same'))
+    model.add(Conv2D(256, (3, 3),strides=(1, 1), padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
+    model.add(Conv2D(512, (3, 3),strides=(1, 1), padding='same'))
+    model.add(Conv2D(512, (3, 3),strides=(1, 1), padding='same'))
+    model.add(Conv2D(512, (3, 3),strides=(1, 1), padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
+    model.add(Conv2D(512, (3, 3),strides=(1, 1), padding='same'))
+    model.add(Conv2D(512, (3, 3),strides=(1, 1), padding='same'))
+    model.add(Conv2D(512, (3, 3),strides=(1, 1), padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
+    #model.add(Flatten())
+    #model.add(Dense(4096))
+    #model.add(Dense(4096))
+    model.add(Conv2D(4096, (7, 7),strides=(1, 1), padding='valid'))
+    model.add(Conv2D(4096, (1, 1),strides=(1, 1), padding='same'))
+    model.add(Conv2DTranspose(512, (7,7), padding='valid'))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2DTranspose(512, (3,3), padding='same'))
+    model.add(Conv2DTranspose(512, (3,3), padding='same'))
+    model.add(Conv2DTranspose(512, (3,3), padding='same'))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2DTranspose(512, (3,3), padding='same'))
+    model.add(Conv2DTranspose(512, (3,3), padding='same'))
+    model.add(Conv2DTranspose(256, (3,3), padding='same'))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2DTranspose(256, (3,3), padding='same'))
+    model.add(Conv2DTranspose(256, (3,3), padding='same'))
+    model.add(Conv2DTranspose(128, (3,3), padding='same'))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2DTranspose(128, (3,3), padding='same'))
+    model.add(Conv2DTranspose(64, (3,3), padding='same'))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2DTranspose(64, (3,3), padding='same'))
+    model.add(Conv2DTranspose(64, (3,3), padding='same'))
+    model.add(Conv2D(5, (1, 1),strides=(1, 1), padding='same', activation='softmax'))
+    model.summary()
+    return model
+
+def load_trained_model_with_weight(weights_path="./model/weights.hdf5", test_x_path="./dataset/dataset224x224/test_x/test.png"):
+   model = create_model()
+   model.load_weights(weights_path)
+   test_x = cv2.imread(test_x_path)
+   predict = model.predict(test_x, verbose=1)
+   print(predict.shape)
+
+
+
+if __name__ == "__main__":
+    load_trained_model_with_weight()
